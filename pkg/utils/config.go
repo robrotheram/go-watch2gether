14f3012d5b1dcd39e00d3d2792bd44f21822d1f7
robrotheram <robrotheram@gmail.com>
2024-02-28 20:42:28 +0000

    Fix config load

--- pkg/utils/config.go
+++ pkg/utils/config.go
@@ -24,20 +24,19 @@ type Config struct {
 }
 
 // LoadConfig reads configuration from file or environment variables.
-func LoadConfig(path string) (err error) {
+func LoadConfig(path string) error {
 	viper.AddConfigPath(path)
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
-
 	viper.AutomaticEnv()
 
-	err = viper.ReadInConfig()
+	err := viper.ReadInConfig()
 	if err != nil {
-		return
+		return err
 	}
 	err = viper.Unmarshal(&Configuration)
 	SetDefaults()
-	return
+	return err
 }
 
 func (c *Config) GetLoglevel() log.Level {
